4d0fee93558744802838a5f16b5a4dffc73453ca
Scott Brackett <scottybrackett@gmail.com>
2023-12-24 10:43:41 -0600

    Update picker.py
    still fixing tabs....

--- picker.py
+++ picker.py
@@ -17,25 +17,28 @@ rand_team = []
 
 # load randomized data
 for t in range(team_count):
-    # pick one at random
-    r = random.randrange(0, len(team))
+	# pick one at random
+	r = random.randrange(0, len(team))
 
-    # add to randomized list
-    rand_team.append(team[r])
+	# add to randomized list
+	rand_team.append(team[r])
 
-    # remove from source list
-    team.remove(team[r])
+	# remove from source list
+	team.remove(team[r])
 
 # output new list
 print(rand_team)
 
 def readCSV():
-    team = []
-    with open('names.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-        team.append(row['first_name'])
-        return team
+	team = []
+
+	# read csv and load into team
+	with open('names.csv', newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+			for row in reader:
+				team.append(row['first_name'])
+				
+	return team
 
 # type name letter by letter
 
