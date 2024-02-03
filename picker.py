c1b4cf9128e2757751b991600fcda80d9d1f88de
scott <scottybrackett@gmail.com>
2024-02-03 17:10:42 +0000

    add first try at team feature

--- picker.py
+++ picker.py
@@ -3,18 +3,25 @@ import csv
 import argparse
 import time
 import sys
- 
+from math import floor
+
 # Initialize parser
 parser = argparse.ArgumentParser()
  
 # Adding optional argument
 parser.add_argument("-o", "--Output", help = "Enter 'one' to set output to one at a time, or 'all' to print all names at once.")
 
+# Adding optional team argument
+parser.add_argument("-t", "--TeamSize", help = "int size to divide teams into")
+
 # read arguments from command line
 args = parser.parse_args()
 
-# assign to var for readability
+# assign output style arg to var
 output_style = args.Output
+
+# assign team size arg to var
+team_size = int(args.TeamSize)
 
 # read csv and return as list
 def readCSV():
@@ -57,7 +64,6 @@ def typePrint(text):
 		sys.stdout.flush()
 		time.sleep(.07*random.randrange(0,5))
 
-
 # print each name to screen
 def printAll(output):
 	for o in output:
@@ -79,9 +85,54 @@ team = readCSV()
 # create randomized list
 rand_team = randomizeList(team)
 
+# create groups and return list of
+# defined group size
+def createTeams(team, size):
+    # take provided size and list of strings
+    # break up into a list of lists
+    num_groups = floor(len(team)/size)
+
+    new_teams = []
+    start = 0
+    end = size
+
+    for i in range(0, num_groups):
+        new_team = team[start:end]
+        new_teams.append(new_team)
+        start = start + size
+        end = end + size
+
+    return new_teams
+
+# define function to create groups
+# and print to screen
+def printTeams(output, team_size):
+    # get list of lists
+    new_team = createTeams(output, team_size)
+    
+    # initialize counter 
+    num = 0
+    # iterate through list and print each team
+    # to console
+    for t in new_team:
+        # increment iteration count 
+        num = num + 1
+        # print team heading
+        print(f"\nteam:{num}\n")
+        
+        # print each member
+        for member in t:
+            typePrint(member)
+            print("\n")
+        print('===================')
+
+    print('\n')
+
 # output new list
 if(output_style == "one"):
 	printOne(rand_team)
+elif(output_style == "team"):
+    printTeams(rand_team, team_size)
 else:
 	printAll(rand_team)
 
